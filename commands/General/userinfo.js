324dfb9b3ee7d395ae2dc455d6bf9a9b11cb7fd0
= <ghuydang@gmail.com>
2020-02-03 00:22:05 -0800

    Added description and proper usage.

--- commands/General/userinfo.js
+++ commands/General/userinfo.js
@@ -7,8 +7,8 @@ module.exports = class extends Command {
       enabled: 'true',
       runIn: ['text'],
       cooldown: 3,
-      description: 'Basic user information!',
-      usage: '(mention:user)',
+      description: 'Get basic user information!',
+      usage: '[mention:user]',
       extendedHelp: 'You can mention another user for their user information. If you don\'t, it\'ll pull out author information.'
     }); 
   }
