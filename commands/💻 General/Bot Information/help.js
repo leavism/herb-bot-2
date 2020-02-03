63effd5dec68899bf825d08fa13e604c31698b18
= <ghuydang@gmail.com>
2020-02-03 11:21:16 -0800

    Refactored and cleaned up.

--- commands/💻 General/Bot Information/help.js
+++ commands/💻 General/Bot Information/help.js
@@ -21,47 +21,13 @@ module.exports = class extends Command {
 
 	async run(message, [command]) {
 		if (command) {
-			// const info = [
-			// 	`= ${command.name} = `,
-			// 	isFunction(command.description) ? command.description(message.language) : command.description,
-			// 	message.language.get('COMMAND_HELP_USAGE', command.usage.fullUsage(message)),
-			// 	message.language.get('COMMAND_HELP_EXTENDED'),
-			// 	isFunction(command.extendedHelp) ? command.extendedHelp(message.language) : command.extendedHelp
-			// ].join('\n');
-			// return message.sendMessage(info, { code: 'asciidoc' });
-			return message.send(await this.buildHelpCommandEmbed(message,command))
+			return message.send(await this.buildCommandEmbed(message,command))
 		}
 		return message.send(await this.buildHelpEmbed(message))
 	}
 
-	// async run(message, [command]) {
-	// 	if (command) {
-	// 		const info = [
-	// 			`= ${command.name} = `,
-	// 			isFunction(command.description) ? command.description(message.language) : command.description,
-	// 			message.language.get('COMMAND_HELP_USAGE', command.usage.fullUsage(message)),
-	// 			message.language.get('COMMAND_HELP_EXTENDED'),
-	// 			isFunction(command.extendedHelp) ? command.extendedHelp(message.language) : command.extendedHelp
-	// 		].join('\n');
-	// 		return message.sendMessage(info, { code: 'asciidoc' });
-	// 	}
-	// 	const help = await this.buildHelp(message);
-	// 	const categories = Object.keys(help);
-	// 	const helpMessage = [];
-	// 	for (let cat = 0; cat < categories.length; cat++) {
-	// 		helpMessage.push(`**${categories[cat]} Commands**:`, '```asciidoc');
-	// 		const subCategories = Object.keys(help[categories[cat]]);
-	// 		for (let subCat = 0; subCat < subCategories.length; subCat++) helpMessage.push(`= ${subCategories[subCat]} =`, `${help[categories[cat]][subCategories[subCat]].join('\n')}\n`);
-	// 		helpMessage.push('```', '\u200b');
-	// 	}
-
-	// 	return message.author.send(helpMessage, { split: { char: '\u200b' } })
-	// 		.then(() => { if (message.channel.type !== 'dm') message.sendLocale('COMMAND_HELP_DM'); })
-	// 		.catch(() => { if (message.channel.type !== 'dm') message.sendLocale('COMMAND_HELP_NODM'); });
-	// }
-
-	async buildHelpCommandEmbed(message, command) {
-		let help = new MessageEmbed()
+	async buildCommandEmbed(message, command) {
+		let commandEmbed = new MessageEmbed()
 			.setTitle(`Command \`\`\`${command.name}\`\`\``)
 			.setDescription(command.description)
 			.addField(
@@ -81,7 +47,7 @@ module.exports = class extends Command {
 			.setFooter(
 				'《 》 aliases │ < > required field │ [ ] optional field '
 			)
-		return help
+		return commandEmbed
 	}
 
 	async buildHelpEmbed(message) { 
@@ -94,41 +60,16 @@ module.exports = class extends Command {
 					all[command.category].push(command)
 				})
 		))
-		const help = new MessageEmbed()
+		const helpEmbed = new MessageEmbed()
 			.setTitle(`${this.client.user.username}`)
 			.setDescription(`To view the commands of each group, use:\n\`\`\`${prefix}commands <group>\`\`\``)
 		Object.keys(all).forEach((category) => {
-			help.addField(
+			helpEmbed.addField(
 				category,
 				`${all[category].length} commands`,
 				true
 			)
 		})
-		return help
+		return helpEmbed
 	}
-
-	async buildHelp(message) {
-		const help = {};
-
-		const { prefix } = message.guildSettings;
-		// These two lines are for padding the message to be evenly spaced
-		const commandNames = [...this.client.commands.keys()];
-		const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-
-		await Promise.all(this.client.commands.map((command) =>
-			this.client.inhibitors.run(message, command, true)
-				.then(() => {
-					if (!has(help, command.category)) help[command.category] = {};
-					if (!has(help[command.category], command.subCategory)) help[command.category][command.subCategory] = [];
-					const description = isFunction(command.description) ? command.description(message.language) : command.description;
-					help[command.category][command.subCategory].push(`• ${command.name.padEnd(longest)} :: ${description}`);
-				})
-				.catch(() => {
-					// noop
-				})
-		));
-
-		return help;
-	}
-
 };
