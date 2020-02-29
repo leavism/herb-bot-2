4439fd9c51e9399578fe2b1c9f7b3ee40828f698
= <ghuydang@gmail.com>
2020-02-29 11:48:35 -0800

    Added developer and Github link.

--- commands/💻 General/Bot Information/stats.js
+++ commands/💻 General/Bot Information/stats.js
@@ -11,19 +11,31 @@ module.exports = class extends Command {
 
   async run (message) {
     return message.sendEmbed(new MessageEmbed()
-      .setTitle(this.client.user.tag)
+      .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL())
       .setColor(0x04ff70)
       .setThumbnail(this.client.user.avatarURL())
-      .addField('🍽️ Serving',
+      .addField(
+        '🍽️ Serving',
         `${this.client.guilds.size} Servers\n${this.client.users.size} Users`,
         true)
-      .addField('🕑 Uptime',
+      .addField(
+        '🕑 Uptime',
         Duration.toNow(Date.now() - (process.uptime() * 1000)),
         true)
       .addField('🧠 Memory',
         `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
         true)
-      .setFooter(`Klasa ${klasaVersion} | Discord.js ${discordVersion} | Node.js ${process.version}`)
+      .addField(
+        'Developer',
+        'Leavism',
+        true
+      )
+      .addField(
+        'GitHub',
+        '[Repository](https://github.com/leavism/Herb-Bot-2.0/tree/master)',
+        true
+      )
+      .setFooter(`Libraries: Klasa ${klasaVersion} | Discord.js ${discordVersion} | Node.js ${process.version}`)
     )
   }
 }
