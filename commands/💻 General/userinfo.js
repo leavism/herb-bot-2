7e21bdace90adcfab06549a58feb385ee8b0c9af
= <ghuydang@gmail.com>
2020-03-01 10:30:52 -0800

    Changed timestamp layout.

--- commands/💻 General/userinfo.js
+++ commands/💻 General/userinfo.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
       usage: '[mention:user]',
       extendedHelp: 'You can mention another user for their user information. If you don\'t, it\'ll pull out author information.'
     })
-    this.timestamp = new Timestamp('d MMMM YYYY')
+    this.timestamp = new Timestamp('MMMM d YYYY')
   }
 
   async run (message, [...params]) {
@@ -24,13 +24,13 @@ module.exports = class extends Command {
     const target = ((message.mentions.users.size === 0) ? message.member : message.mentions.members.first())
     const createdDaysAgo = `(${Math.round((new Date() - target.user.createdAt) / (24 * 60 * 60 * 1000))} days ago)`
     const joinedDaysAgo = `(${Math.round((new Date() - target.joinedAt) / (24 * 60 * 60 * 1000))} days ago)`
-    const roles = (target.roles.size === 1) ? 'No roles :(' : target.roles.map(m => m).filter(m => m.name !== '@everyone').join(' | ')
+    const roles = (target.roles.size === 1) ? 'No roles :(' : target.roles.map(m => m).filter(m => m.name !== '@everyone').join(' ')
     const sortedMembers = message.guild.members.sort(this.compareJoinedAt).map(m => m.user)
 
     const userInfoEmbed = new MessageEmbed()
-      .setAuthor(target.user.tag, target.user.displayAvatarURL())
+      .setAuthor(`${target.user.tag} ${(target.nickname) ? `(${target.nickname})` : ''}`, target.user.displayAvatarURL())
       .setThumbnail(target.user.displayAvatarURL())
-      .setDescription(`Status: ${target.user.presence.status.toUpperCase()}`)
+      .setDescription(`${target.user}`)
       .setColor(message.member.displayHexColor)
       .addField(
         'Joined Discord on',
