6065d152a250938b2959cc9499d7b0961a20bad2
= <ghuydang@gmail.com>
2020-03-02 20:39:20 -0800

    Prevented hitting the SQL max int value.

--- commands/💎 Shop/Admins/award.js
+++ commands/💎 Shop/Admins/award.js
@@ -15,19 +15,35 @@ module.exports = class extends Command {
 
   async run (message, [member, amount]) {
     if (await this.checkUser(member) === false) await this.makeUser(member)
-    await this.awardUser(member, amount)
-    return message.send(`${member} has been awarded ${amount} Simbits!`)
+    const awarded = await this.awardUser(member, amount)
+    if (awarded) {
+      return message.send(`${member} has been awarded ${amount} Simbits!`)
+    } else {
+      return message.send('That would exceed the maximum balance of 2147483647 Simbits.')
+    }
   }
 
+  /**
+   * Add guild member into the user table
+   * @param {guildMember} memberObj - The target guild member
+   */
   async makeUser (memberObj) {
     const startingBalance = 0
     await this.db.run(`INSERT INTO user (discord_id, balance) VALUES (${memberObj.id}, ${startingBalance})`)
   }
 
   async awardUser (memberObj, amount) {
-    return this.db.run(`UPDATE user SET balance = balance + ${amount} WHERE discord_id = ${memberObj.id}`)
+    const shopUser = await this.db.get('user', 'discord_id', memberObj.id)
+    if (shopUser.balance + amount >= 2147483647) return false
+    this.db.run(`UPDATE user SET balance = balance + ${amount} WHERE discord_id = ${memberObj.id}`)
+    return true
   }
 
+  /**
+   * Checks if guild member is in the user table
+   * @param {guildMember} memberObj - The target guild member
+   * @returns {boolean} - Whether guild member is in user table (true) or not in the table (false)
+   */
   async checkUser (memberObj) {
     const result = await this.db.get('user', 'discord_id', memberObj.id)
     if (result == null) {
