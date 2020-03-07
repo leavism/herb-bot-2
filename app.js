f4b25139bb5a487d946d63d23a4b15f2fc94af7a
= <ghuydang@gmail.com>
2020-03-06 21:47:58 -0800

    Refactor.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const config = require('./config.js');
   client.permissionLevels = new PermissionLevels()
     .add(0, () => true)
     .add(5, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('regular').name), { fetch: true })
-    .add(7, ({ guild, member }) => (guild && member.roles.find(role => role.name.toLowerCase().includes('moderator')).name), { fetch: true })
+    .add(7, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('moderator').name), { fetch: true })
     .add(9, ({ guild, member }) => guild && member.permissions.has('ADMINISTRATOR'), { fetch: true })
     .add(10, ({ author }) => author === client.owner)
 
