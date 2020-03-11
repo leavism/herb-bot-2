a5afc292e34e393ef5c5bc912574e6cc012bd5d5
= <ghuydang@gmail.com>
2020-03-10 17:02:11 -0700

    Implement roles commands.

---
+++ commands/👀Admin/Join Roles/rtoggle.js
@@ -0,0 +1,36 @@
+const { Command } = require('klasa')
+const jRoles = require('../../../data/joinable.json')
+const fs = require('fs')
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'rtoggle',
+      enabled: true,
+      aliases: ['rt', 'roletoggle'],
+      permissionLevel: 7,
+      description: 'Toggle where people can use the role command on a role.',
+      usage: '<Role:role>',
+      extendedHelp: ''
+    })
+  }
+
+  async run (message, [role]) {
+    if (jRoles.joinable.includes(role.name)) {
+      jRoles.joinable.splice(jRoles.joinable.indexOf(role.name), 1)
+      return message.send(`${role.name} is no longer jonable. Here's a list of roles people can joinse: ${this.buildRoleList()}`)
+    } else {
+      jRoles.joinable.push(role.name)
+      await this.saveJoinables()
+      return message.send(`${role.name} is now joinable. Here's a list of roles people can join: ${this.buildRoleList()}`)
+    }
+  }
+
+  async saveJoinables () {
+    return fs.writeFile('./data/joinable.json', JSON.stringify(jRoles), err => console.log(err))
+  }
+
+  buildRoleList () {
+    return `\`\`\`${jRoles.joinable.join('\n@')}\`\`\``
+  }
+}
---
+++ commands/💻 General/role.js
@@ -0,0 +1,34 @@
+const { Command } = require('klasa')
+const jRoles = require('../../data/joinable.json')
+const fs = require('fs')
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'role',
+      enabled: true,
+      description: 'Join or leave a role. If you already have a role, this will remove it.',
+      usage: '<Role:...string>',
+      extendedHelp: 'Do not mention the role you\'re trying to join.'
+    })
+  }
+
+  async run (message, [targetRole]) {
+    if (!message.member.manageable) return message.send('You have a higher permission than me!')
+
+    const roleExist = message.guild.roles.find(role => role.name.toLowerCase() === targetRole.toLowerCase())
+    if (!roleExist) return message.send(`'${targetRole}' isn't a role on this server.`)
+
+    const joinableRole = jRoles.joinable.find(role => role.toLowerCase() === roleExist.name.toLowerCase())
+    if (!joinableRole) return message.send(`You aren't allowed manage the '${targetRole}' role.`)
+
+    const alreadyHave = message.member.roles.find(role => role === roleExist)
+    if (alreadyHave) {
+      message.member.roles.remove(roleExist, `${message.member} used the leave command.`)
+      return message.send(`I've removed the ${roleExist.name} role.`)
+    } else {
+      message.member.roles.add(roleExist, `${message.member} used the join command.`)
+      return message.send(`You now have the ${roleExist.name} role!`)
+    }
+  }
+}
