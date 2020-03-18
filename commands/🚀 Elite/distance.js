4127140ea4d2c698afad7a467a637feef7c3c335
Giahuy Dang <leavism@Freya.lan>
2020-03-17 17:40:11 -0700

    Relies on SQL database, and uses EDSM as backup.

--- commands/🚀 Elite/distance.js
+++ commands/🚀 Elite/distance.js
@@ -13,39 +13,44 @@ module.exports = class extends Command {
       usageDelim: ',',
       extendedHelp: 'Don\'t forget the comma between the two system names.'
     })
-
-    this.createCustomResolver('system', (arg, possible, message) => {
-      return this.client.arguments.get('system').run(arg, possible, message)
-    })
+    this.db = this.client.providers.get('jegin')
   }
 
   async run (message, [systemA, systemB]) {
-    const systemACoords = await this.getCoords(systemA.trim())
-    const systemBCoords = await this.getCoords(systemB.trim())
+    systemA = await this.getCoords(systemA) || await this.getEDSMCoords(systemA)
+    systemB = await this.getCoords(systemB) || await this.getEDSMCoords(systemB)
+    if (systemA === null) return message.send(`I couldn't find coordinates for the '${systemA}' system.`)
+    if (systemB === null) return message.send(`I couldn't find coordinates for the '${systemA}' system.`)
 
-    // EDSM does not have HTTP statuses, so they all return code 200
-    if (Array.isArray(systemACoords)) {
-      throw `Could not find coordinates for '${systemA}' system.`
-    } else if (Array.isArray(systemBCoords)) {
-      throw `Could not find coordinates for '${systemB}' system.`
-    }
-
-    const x = systemACoords.coords.x - systemBCoords.coords.x
-    const y = systemACoords.coords.y - systemBCoords.coords.y
-    const z = systemACoords.coords.z - systemBCoords.coords.z
+    const x = systemA.x - systemB.x
+    const y = systemA.y - systemB.y
+    const z = systemA.z - systemB.z
 
     const distance = Math.sqrt(x * x + y * y + z * z)
-    await message.send(`The distance between ${systemACoords.name} and ${systemBCoords.name} is **${distance.toFixed(2)}**LY`)
+    await message.send(`The distance between ${systemA.name} and ${systemB.name} is **${distance.toFixed(2)}**LY`)
   };
 
   /**
    * Calls the EDSM API to find the coordinates of a system
    * @param {string} system - The name of the system
    */
-  async getCoords (system) {
+  async getEDSMCoords (system) {
     system = system.replace('+', '%2B').replace(' ', '+')
     const url = `https://www.edsm.net/api-v1/system?sysname=${system}&coords=1`
     const response = await fetch(url)
-    return response.json()
+      .then(response => response.json())
+
+    if (Array.isArray(response)) return null
+
+    return {
+      name: response.name,
+      x: response.coords.x,
+      y: response.coords.y,
+      z: response.coords.z
+    }
+  }
+
+  async getCoords (system) {
+    return this.db.get('system', 'name', system)
   }
 }
