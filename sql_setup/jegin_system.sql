642ab73c913400c17db6e33c643299d29762295f
Giahuy Dang <leavism@Giahuys-MBP.lan>
2020-03-27 19:10:04 -0700

    Create db if it doesn't exist.

--- sql_setup/jegin_system.sql
+++ sql_setup/jegin_system.sql
@@ -18,6 +18,8 @@
 --
 -- Table structure for table `system`
 --
+CREATE DATABASE  IF NOT EXISTS `jegin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `jegin`;
 
 DROP TABLE IF EXISTS `system`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
