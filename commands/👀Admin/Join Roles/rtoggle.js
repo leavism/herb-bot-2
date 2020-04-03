961875a2a1065e15fb96716f9e430ab03cba97a5
Giahuy Dang <ghuydang@gmail.com>
2020-04-03 01:04:30 -0700

    Refactored for better variable names.

--- commands/👀Admin/Join Roles/rtoggle.js
+++ commands/👀Admin/Join Roles/rtoggle.js
@@ -1,5 +1,5 @@
 const { Command } = require('klasa')
-const jRoles = require('../../../data/joinable.json')
+const roleList = require('../../../data/joinable.json')
 const fs = require('fs')
 
 module.exports = class extends Command {
@@ -16,27 +16,30 @@ module.exports = class extends Command {
     this.managerial = ["ADMINISTRATOR", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "MANAGE_ROLES", "MANAGE_WEBHOOKS"];
   }
 
-  async run (message, [targetRole]) {
-    if (!targetRole) return message.send(`Here is a list of roles people can join: ${this.buildRoleList()} Use \`${this.client.options.prefix}rtoggle <role_name>\` if you want to toggle a role.`)
-    const roleExist = message.guild.roles.find(role => role.name.toLowerCase() === targetRole.toLowerCase())
-    if (!roleExist) return message.send(`'${targetRole}' isn't a role on this server.`)
-    const roleManagerial = roleExist.permissions.toArray().some(role => this.managerial.includes(role))
-    if (roleManagerial) return message.send(`'${roleExist.name}' has a managerial persmission. It isn't wise to toggle this role.`)
-    if (jRoles.joinable.includes(roleExist.name)) {
-      jRoles.joinable.splice(jRoles.joinable.indexOf(roleExist.name), 1)
-      return message.send(`'${roleExist.name}' is no longer jonable. Here's a list of roles people can join: ${this.buildRoleList()}`)
+  async run (message, [roleName]) {
+    if (!roleName) return message.send(`Here is a list of roles people can join: ${this.buildRoleList()} Use \`${this.client.options.prefix}rtoggle <role_name>\` if you want to toggle a role.`)
+
+    const role = message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase())
+    if (!role) return message.send(`'${roleName}' isn't a role on this server.`)
+
+    const isManagerial = role.permissions.toArray().some(role => this.managerial.includes(role))
+    if (isManagerial) return message.send(`'${role.name}' has a managerial persmission. It isn't wise to toggle this role.`)
+    
+    if (roleList.joinable.includes(role.name)) {
+      roleList.joinable.splice(roleList.joinable.indexOf(role.name), 1)
+      return message.send(`'${role.name}' is no longer jonable. Here's a list of roles people can join: ${this.buildRoleList()}`)
     } else {
-      jRoles.joinable.push(roleExist.name)
-      await this.saveJoinables()
-      return message.send(`'${roleExist.name}' is now joinable. Here's a list of roles people can join: ${this.buildRoleList()}`)
+      roleList.joinable.push(role.name)
+      await this.saveRoleList()
+      return message.send(`'${role.name}' is now joinable. Here's a list of roles people can join: ${this.buildRoleList()}`)
     }
   }
 
-  async saveJoinables () {
-    return fs.writeFile('./data/joinable.json', JSON.stringify(jRoles), err => console.log(err))
+  async saveRoleList () {
+    return fs.writeFile('./data/joinable.json', JSON.stringify(roleList), err => console.log(err))
   }
 
   buildRoleList () {
-    return `\`\`\`${jRoles.joinable.join('\n')}\`\`\``
+    return `\`\`\`${roleList.joinable.join('\n')}\`\`\``
   }
 }
