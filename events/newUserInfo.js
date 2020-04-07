19b751c9bb7e206b35043eee936b453f925b0184
Giahuy Dang <ghuydang@gmail.com>
2020-04-07 09:39:43 -0700

    Added comments.

--- events/newUserInfo.js
+++ events/newUserInfo.js
@@ -19,13 +19,17 @@ module.exports = class extends Event {
     if (await this.checkUser(member) === false) await this.makeUser(member)
     const user = await this.getUser(member)
     await this.updateJoinTable({ user, member })
+
     const joinLogChannelName = await this.db.get('config', 'key', 'join_log_channel')
     const joinLogChannel = member.guild.channels.find(channel => channel.name === joinLogChannelName.value)
     return joinLogChannel.send(await this.buildTargetInfoEmbed(member))
   }
 
+  /**
+   * Builds the message embed for when a new user joins the Discord
+   * @param {GuildMember} memberObj Target guild member
+   */
   async buildTargetInfoEmbed (memberObj) {
-    // If no user mention, then info pulled about author
     const createdDaysAgo = `(${Math.round((new Date() - memberObj.user.createdAt) / (24 * 60 * 60 * 1000))} days ago)`
     const joinedDaysAgo = `(${Math.round((new Date() - memberObj.joinedAt) / (24 * 60 * 60 * 1000))} days ago)`
 
@@ -62,14 +66,26 @@ module.exports = class extends Event {
     await this.db.run(`INSERT INTO user (discord_id, balance) VALUES (${memberObj.id}, ${startingBalance})`)
   }
 
+  /**
+   * Updates the databate join table
+   * @param {objectLiteral} data - Contains the database user object and the Discord member object
+   */
   async updateJoinTable (data) {
     await this.db.run(`INSERT INTO simbad.join (user_id, date) VALUES (${data.user.id}, '${this.localISOTime(data.member.joinedAt)}')`)
   }
 
+  /**
+   * Gets all the rows in the database join table of a specific user
+   * @param {userObj} user - Database user object
+   */
   async getJoinDates (user) {
     return this.db.runAll(`SELECT * FROM simbad.join WHERE user_id = ${user.id}`)
   }
 
+  /**
+   * Puts all the previous join dates into a string
+   * @param {userObj} user - Database user object
+   */
   async stringifyJoinDates (user) {
     const joinDates = await this.getJoinDates(user)
     return joinDates.map(obj => `${this.timestamp.display(obj.date)}`).join(', ')
@@ -96,9 +112,13 @@ module.exports = class extends Event {
     return true
   }
 
-  localISOTime (d) {
-    if (!d) d = new Date()
-    var tzoffset = d.getTimezoneOffset() * 60000 // offset in milliseconds
-    return (new Date(d - tzoffset)).toISOString().slice(0, 19).replace('T', ' ')
+  /**
+   * Converts ISOTime of date object to current timezome, keeping the ISO format
+   * @param {Date} date - Date object
+   */
+  localISOTime (date) {
+    if (!date) date = new Date()
+    var tzoffset = date.getTimezoneOffset() * 60000 // offset in milliseconds
+    return (new Date(date - tzoffset)).toISOString().slice(0, 19).replace('T', ' ')
   }
 }
