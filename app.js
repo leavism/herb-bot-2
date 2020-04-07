07eec7bed643c90c383f0f01fc313997f3846628
Giahuy Dang <ghuydang@gmail.com>
2020-04-06 18:15:42 -0700

    Added permission levels for people with any role and people with Simbian role.

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const config = require('./config.js');
 
   client.permissionLevels = new PermissionLevels()
     .add(0, () => true)
-    .add(5, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('regular').name), { fetch: true })
-    .add(7, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('moderator').name), { fetch: true })
+    .add(1, ({ guild, member }) => guild && member.roles.array().length > 1) // Had a role other than @everyone
+    .add(2, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('simbian')), { fetch: true})
+    .add(5, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('regular')), { fetch: true })
+    .add(7, ({ guild, member }) => guild && member.roles.find(role => role.name.toLowerCase().includes('moderator')), { fetch: true })
     .add(9, ({ guild, member }) => guild && member.permissions.has('ADMINISTRATOR'), { fetch: true })
     .add(10, ({ author }) => author === client.owner)
 
